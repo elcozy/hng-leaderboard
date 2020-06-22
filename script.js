206b9896c1a91629bd9b2ddf0775d17d902a91d3
petrepan <peterawotola@gmail.com>
2020-06-22 22:26:49 +0100

    sm share link added

--- script.js
+++ script.js
@@ -32,15 +32,20 @@ function appendData(data) {
 
         var tableRef = document.getElementById("userDataBody")
         var row = tableRef.insertRow(i);
-
+        // console.log(tableRef)
         row.insertCell(0).innerHTML = fullName;
         userDataBody.className = "userDataBody";
         row.insertCell(1).innerHTML = userName;
         row.insertCell(2).innerHTML = email;
         row.insertCell(3).innerHTML = totalPoints;
-
+        row.insertCell(
+          4
+        ).innerHTML = `<a href="https://twitter.com/intent/tweet?text=My%20Total%20Point%20on%20HNG%20Leader%20board%20is ${totalPoints}" target="_blank">Share on Twiter</a>`;
+    
     }
 
 }
+
+
 
 // console.log(object)
