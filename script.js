0c6c28cb40da7c30a01e22c43072718be618f73e
ifeakins <elcozy@users.noreply.github.com>
2020-06-22 22:18:07 +0100

    🎇

--- script.js
+++ script.js
@@ -24,20 +24,26 @@ function appendData(data) {
     for (var i = 0; i < obj.length; i++) {
 
 
+
         var fullName = obj[i]["fullName"]
         var userName = obj[i]["userName"]
         var email = obj[i]["email"]
         var totalPoints = obj[i]["totalPoints"]
 
+        if (i < 3) {
+            let top3 = document.getElementById("first3").getElementsByClassName(`top${i + 1}`)[0]
+            top3.innerHTML = `<img src="./praise${i}.png" alt=""> <h4> ${fullName}</h4> <h5>Slack ID: ${userName}</h5> <h3>${totalPoints} Points</h3><img src="./${i}.png" alt="">`
+        }
 
         var tableRef = document.getElementById("userDataBody")
         var row = tableRef.insertRow(i);
 
-        row.insertCell(0).innerHTML = fullName;
+        row.insertCell(0).innerHTML = i + 1;
+        row.insertCell(1).innerHTML = fullName;
         userDataBody.className = "userDataBody";
-        row.insertCell(1).innerHTML = userName;
-        row.insertCell(2).innerHTML = email;
-        row.insertCell(3).innerHTML = totalPoints;
+        row.insertCell(2).innerHTML = userName;
+        row.insertCell(3).innerHTML = email;
+        row.insertCell(4).innerHTML = totalPoints;
 
     }
 
